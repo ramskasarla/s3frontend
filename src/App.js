aba2138bf043df5d1b67414b2f3105bd17cbcd07
doctorqp <govadarajesh@gmail.com>
2022-03-14 21:06:51 +0530

    initialize repo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Java<code> Application</code> with reactjs frontend
+        </p>
+        <a
+          className="App-link"
+          href="https://snakes.poc6.ga"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          POC 6
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
